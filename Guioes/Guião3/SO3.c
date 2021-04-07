e3c0e17023c974247826cb8699c2beb46c1a8726
IvoLims <75138193+IvoLims@users.noreply.github.com>
2021-04-07 17:01:01 +0100

    Update SO3.c

--- Guioes/Guião3/SO3.c
+++ Guioes/Guião3/SO3.c
@@ -84,7 +84,7 @@ int main(int argc,char* argv[]){
     return 0;
 }
 
-/* 6. Implemente uma versao simplificada da função system(). Ao contrário da função original, não tente
+/* 6. Implemente uma versão simplificada da função system(). Ao contrário da função original, não tente
 suportar qualquer tipo de redireccionamento, ou composição/encadeamento de programas executáveis.
 O único argumento deverá ser uma string que específica um programa executável e uma eventual lista de
 argumentos. Procure que o comportamento e valor de retorno da sua funçao sejam compatíveis com a
