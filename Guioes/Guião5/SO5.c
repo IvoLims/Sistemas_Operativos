1164318775a55db48b41151d06b5d2a989a6edbf
IvoLims <75138193+IvoLims@users.noreply.github.com>
2021-04-21 10:13:12 +0100

    Update SO5.c

--- Guioes/Guião5/SO5.c
+++ Guioes/Guião5/SO5.c
@@ -74,3 +74,51 @@ int main(int argc,char* argv[]){
     filho2pai()
     return 0;
 }
+
+//2
+
+int pai2filhoV2(){
+  int p[2]; //p[0] -> ler; p[1] -> escrever;
+  char line[] = "aqui\n";
+  char buffer[20];
+  int res;
+  int statues;
+  if (pipe(p) != 0){
+    perror("Error");
+    return -1;
+  }
+  switch(fork()){
+    case -1:{
+      perror("Fork");
+      return -1;
+    }
+    case 0:{ //Filho
+      //fechar filho
+      close(p[1]);
+      while(read(p[0],&buffer,1) > 0){
+            printf("Filho leu: %s\n",buffer);
+      }
+      printf("Filho leu: %s",buffer);
+      close(p[0]);
+      _exit(0);
+    }
+    default:{ //Pai
+      //fechar pai
+      close(p[0]);
+      //sleep(5);
+      for(int i = 0;i < 1;i++){
+          write(p[1], &line, sizeof(line));
+          printf("Pai escreveu no pipe\n");
+      }
+      close(p[1]);
+      wait(&statues);
+    }
+  }
+  return 0;
+}
+
+int main(int argc,char* argv[]){
+    pai2filhoV2();
+    return 0;
+}
+
