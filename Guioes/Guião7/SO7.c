b7b8e953b2aca30d6b955022c332c2342712aa32
IvoLims <75138193+IvoLims@users.noreply.github.com>
2021-05-19 09:57:07 +0100

    Create SO7.c

---
+++ Guioes/Guião7/SO7.c
@@ -0,0 +1,33 @@
+//1
+unsigned int count = 0;
+unsigned int seconds = 0;
+
+void sig_handler(int signum){
+  seconds++;
+}
+void quit_handler(int signum){
+  printf(" Recebi sinal %d, o CTR+C foi chamado %d vezes.\n",signum,count);
+  exit(0);
+}
+void int_handler(int signum){
+  count++;
+  printf(" Recebi o sinal %d e passaram-se %d segundos",signum,seconds);
+}
+
+int main(int argc,char* argv[]){
+  if(signal(SIGALRM,sig_handler)){
+    perror("SIGALRM");
+  }
+  if(signal(SIGQUIT,quit_handler)){
+    perror("SIGALRM");
+  }
+  if(signal(SIGINT,int_handler)){
+    perror("SIGALRM");
+  }
+  do{
+    alarm(1);
+    pause();
+    printf("tic\n");
+  }while(1);
+  return 0;
+}
