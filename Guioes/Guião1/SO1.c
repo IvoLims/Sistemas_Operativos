c99b6e6ad363535309768faf057db2ad5e342592
IvoLims <75138193+IvoLims@users.noreply.github.com>
2022-02-16 16:03:37 +0000

    Update SO1.c

--- Guioes/Guião1/SO1.c
+++ Guioes/Guião1/SO1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 #include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
 #include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
